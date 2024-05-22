7d065f964cce88f8c3edbb4299438088bb70c430
Eduardo Valencia <EduardoVG@proton.me>
2024-05-22 15:18:42 -0600

    refactor: Add gradient background to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import styled from "styled-components";
+
+const GradientBg = styled.div`
+  background: linear-gradient(
+    17deg,
+    #041833 4.16%,
+    #04244f 48%,
+    #154580 96.76%
+  );
+  width: 100%;
+  min-height: 100dvh;
+`;
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <GradientBg />
+    </>
+  );
 }
 
 export default App;
